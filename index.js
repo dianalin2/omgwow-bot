da6de577893e03299c4eedc15f69a1f526d53f62
Diana Lin <lindiana1206@gmail.com>
2021-05-20 21:10:30 -0400

    feat: basic single-word responses

--- index.js
+++ index.js
@@ -1,15 +1,67 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+// TODO: Implement DB
+const triggers = {}
+
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+    if (msg.author.bot)
+        return;
+
+    if (!triggers[msg.guild.id])
+        triggers[msg.guild.id] = {};
+
+    for (const s of Object.keys(triggers[msg.guild.id])) {
+        if (msg.content.includes(s)) {
+            msg.reply(triggers[msg.guild.id][s]);
+        }
     }
+
+    if (msg.content.startsWith('~')) {
+        const command = msg.content.substring(1).match(/[^ ]+/g);
+        console.log(command)
+        switch (command[0]) {
+            case 'responses':
+                if (command.length < 2) {
+                    msg.reply('Usage: ~responses add|del ...');
+                    return;
+                }
+
+                if (command[1] === 'add') {
+                    if (command.length != 4) {
+                        msg.reply('Usage: ~responses add <trigger> <response>');
+                        return;
+                    }
+
+                    triggers[msg.guild.id][command[2]] = command[3];
+                    msg.reply(`New response added:\nTrigger: ${command[2]}\nResponse: ${command[3]}`);
+                } else if (command[1] === 'del') {
+                    if (command.length != 4) {
+                        msg.reply('Usage: ~responses del <trigger>');
+                        return;
+                    }
+
+                    if (!triggers[msg.guild.id])
+                        triggers[msg.guild.id] = {};
+
+                    if (!triggers[msg.guild.id[command[2]]]) {
+                        msg.reply(`Response doesn't exist!`);
+                    }
+
+                    const res = triggers[msg.guild.id][command[2]];
+                    delete triggers[msg.guild.id][command[2]];
+                    msg.reply(`Response removed:\nTrigger: ${command[2]}\nResponse: ${res}`);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
 });
 
-// bad
-client.login('ODQ1MDc0OTM4MTEyMjQ1ODIw.YKbrXQ.Tfp0GLgaMiGeX15dI6ZKEFiy_rA');
+client.login(process.env.DISCORD_TOKEN);
