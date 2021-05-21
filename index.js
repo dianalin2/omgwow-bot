40838a1c9202598e8a496f72fd2edfcee8f00ec2
Diana Lin <lindiana1206@gmail.com>
2021-05-20 21:54:43 -0400

    fix: add support for multiple-word args

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const client = new Discord.Client();
 const dotenv = require('dotenv');
 dotenv.config();
 
+const { parse } = require('discord-command-parser');
+
 // TODO: Implement DB
 const triggers = {}
 
@@ -12,57 +14,62 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.author.bot)
-        return;
+
+    const parsed = parse(msg, "~", { allowSpaceBeforeCommand: true });
 
     if (!triggers[msg.guild.id])
         triggers[msg.guild.id] = {};
 
-    for (const s of Object.keys(triggers[msg.guild.id])) {
-        if (msg.content.includes(s)) {
-            msg.reply(triggers[msg.guild.id][s]);
+    if (!parsed.success && msg.author.id !== client.user.id) {
+        for (const s of Object.keys(triggers[msg.guild.id])) {
+            if (msg.content.includes(s)) {
+                msg.reply(triggers[msg.guild.id][s]);
+            }
         }
+
+        return;
     }
 
-    if (msg.content.startsWith('~')) {
-        const command = msg.content.substring(1).match(/[^ ]+/g);
-        console.log(command)
-        switch (command[0]) {
-            case 'responses':
-                if (command.length < 2) {
-                    msg.reply('Usage: ~responses add|del ...');
+    const args = parsed.arguments;
+
+    console.log(parsed.command);
+    console.log(args);
+    switch (parsed.command) {
+        case 'responses':
+
+            if (args.length < 2) {
+                msg.reply('Usage: ~responses add|del ...');
+                return;
+            }
+
+            if (args[0] === 'add') {
+                if (args.length !== 3) {
+                    msg.reply('Usage: ~responses add <trigger> <response>');
                     return;
                 }
 
-                if (command[1] === 'add') {
-                    if (command.length != 4) {
-                        msg.reply('Usage: ~responses add <trigger> <response>');
-                        return;
-                    }
-
-                    triggers[msg.guild.id][command[2]] = command[3];
-                    msg.reply(`New response added:\nTrigger: ${command[2]}\nResponse: ${command[3]}`);
-                } else if (command[1] === 'del') {
-                    if (command.length != 4) {
-                        msg.reply('Usage: ~responses del <trigger>');
-                        return;
-                    }
-
-                    if (!triggers[msg.guild.id])
-                        triggers[msg.guild.id] = {};
-
-                    if (!triggers[msg.guild.id[command[2]]]) {
-                        msg.reply(`Response doesn't exist!`);
-                    }
-
-                    const res = triggers[msg.guild.id][command[2]];
-                    delete triggers[msg.guild.id][command[2]];
-                    msg.reply(`Response removed:\nTrigger: ${command[2]}\nResponse: ${res}`);
+                triggers[msg.guild.id][args[1]] = args[2];
+                msg.reply(`New response added:\nTrigger: ${args[1]}\nResponse: ${args[2]}`);
+            } else if (args[0] === 'del') {
+                if (args.length !== 3) {
+                    msg.reply('Usage: ~responses del <trigger>');
+                    return;
                 }
-                break;
-            default:
-                break;
-        }
+
+                if (!triggers[msg.guild.id])
+                    triggers[msg.guild.id] = {};
+
+                if (!triggers[msg.guild.id[args[1]]]) {
+                    msg.reply(`Response doesn't exist!`);
+                }
+
+                const res = triggers[msg.guild.id][args[1]];
+                delete triggers[msg.guild.id][args[1]];
+                msg.reply(`Response removed:\nTrigger: ${args[1]}\nResponse: ${res}`);
+            }
+            break;
+        default:
+            break;
     }
 
 });
